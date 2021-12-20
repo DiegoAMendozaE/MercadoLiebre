662d8adb4cdaa9c4fec54e4993134529ad0ac671
DiegoAMendozaE <damendozae@correo.udistrital.edu.co>
2021-12-20 10:29:14 -0500

    stilos

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const publicPath= path.resolve(__dirname,'./public')
 //definimos la carpeta de los archibos estaticos 
 app.use(express.static(publicPath))
 
-//definimos nuestro archibo de entrada 
-app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname,'./views/home.html')))
+
 //habilitamos el puerto
 app.listen(3000,()=>{console.log("Servidor arriba en puesto ")})
+//definimos nuestro archibo de entrada 
+app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname,'./views/home.html')))
+app.post('/',(req,res)=>res.sendFile(path.resolve(__dirname,'./views/home.html')))
+app.get('/register',(req,res)=>res.sendFile(path.resolve(__dirname,'./views/register.html')))
+app.get('/login',(req,res)=>res.sendFile(path.resolve(__dirname,'./views/login.html')))
+
 
 
 
